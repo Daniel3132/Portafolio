a36e060e74bbbb2a8cb65d7a3b4ebb18b213ff31
Daniel3132 <danielcorrea3131@gmail.com>
2022-08-22 11:23:44 -0500

    avatar

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,26 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import avatar from '../assets/avatar.png'
 
 const Intro = () => {
 
-    const changeTheme = () => {
-        const body = document.getElementById('body')
-
-        if (body.classList.contains('dark')) {
-            body.classList.remove('dark')
-            body.classList.add('light')
-
-        } else if (body.classList.contains('light')) {
-            body.classList.remove('light')
-            body.classList.add('dark')
-        }
-    }
+    const [imagen, setImagen] = useState(true);
 
     return (
         <section id='intro'>
             <h1>Daniel Correa - Desarrollador Frontend</h1>
             <div className='introText'>
-                <img className='imgProfile' src='https://res.cloudinary.com/dcyn2bjb9/image/upload/v1653346546/samples/yo/WhatsApp_Image_2020-11-07_at_17.29.56-modified_ljvdzi.png' alt="" />
-
+                {
+                    imagen ?
+                        <img onMouseOver={()=>setImagen(!imagen)} className='imgProfile' src='https://res.cloudinary.com/dcyn2bjb9/image/upload/v1653346546/samples/yo/WhatsApp_Image_2020-11-07_at_17.29.56-modified_ljvdzi.png' alt="" />
+                        :
+                        <img onMouseOver={()=>setImagen(!imagen)} className='imgProfile avatar' src={avatar} alt="" />
+                }
                 <p>Hola, soy un apasionado de la tecnología, la música y los videojuegos, estudio programación y hago proyectos como desarrollador Frontend con: Javascript, HTML y CSS, React - Redux...</p>
             </div>
 
